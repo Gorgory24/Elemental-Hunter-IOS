2e779780a251583395de09cd5f8617c141d900a9
etudiant <etudiant@imac-de-admin-2.local>
2019-10-21 13:51:04 +0400

    début

--- Elemental Hunter/ViewController.swift
+++ Elemental Hunter/ViewController.swift
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var ExitButton: UIButton!
+    
+    @IBAction func Exit(_ sender: Any) {
+        exit(0)
+    }
 }
 
